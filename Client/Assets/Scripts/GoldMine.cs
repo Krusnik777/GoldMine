80f91bcd944457190059169ef2228d73bf38b079
Krusnik777 <132759904+Krusnik777@users.noreply.github.com>
2024-05-30 00:03:28 +0300

    Update GoldMine.cs

--- Client/Assets/Scripts/GoldMine.cs
+++ Client/Assets/Scripts/GoldMine.cs
@@ -19,6 +19,8 @@ public class GoldMine : MonoBehaviour
 
     private void Update()
     {
+        if (readyToMine) return;
+    
         timer += Time.deltaTime;
 
         if (timer > m_timeForMine)
